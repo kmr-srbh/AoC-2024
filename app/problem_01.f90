df49a367b018d1ff90785fbc898a71c1274c08c8
Jacob Williams <jacobwilliams@users.noreply.github.com>
2024-11-30 23:16:29 -0600

    day1

--- app/problem_01.f90
+++ app/problem_01.f90
@@ -9,46 +9,34 @@ integer :: iunit
 logical :: status_ok
 character(len=:),allocatable :: line
 integer :: n_lines
-integer :: i, j, k, n, isum, ifirst, ilast, idxfirst, idxlast
-
-character(len=*),dimension(9),parameter :: ichars = [ &
-    'one  ', &
-    'two  ', &
-    'three', &
-    'four ', &
-    'five ', &
-    'six  ', &
-    'seven', &
-    'eight', &
-    'nine ']
+integer :: i, j, isum
+integer,dimension(:),allocatable :: a, b
+type(string),dimension(:),allocatable :: vals
 
 call clk%tic()
 
+! -------- part 1 -----------
 isum = 0
 open(newunit=iunit, file='inputs/day1.txt', status='OLD')
 n_lines = number_of_lines_in_file(iunit)
+allocate(a(n_lines), b(n_lines))
 do i = 1, n_lines
-
-    ! remove all the chars before the first and after the last number
     line = read_line(iunit,status_ok)
-    n = len(line)
-
+    read(line, *) a(i), b(i)
+end do
+call sort(a)
+call sort(b)
+isum = 0
+do i = 1, n_lines
+    isum = isum + abs(b(i) - a(i))
 end do
 write(*,*) '1a: sum:', isum
 close(iunit)
 
-
 ! -------- part 2 -----------
-
-open(newunit=iunit, file='inputs/day1.txt', status='OLD')
-n_lines = number_of_lines_in_file(iunit)
 isum = 0
 do i = 1, n_lines
-
-    line = read_line(iunit,status_ok)
-    n = len(line)
-
-
+    isum = isum + a(i)*count(a(i)==b)
 end do
 write(*,*) '1b: sum:', isum
 
